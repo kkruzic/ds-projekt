bc883078c494c9aa7e69a175d5af2bbf6af75f63
kkruzic <58296882+kkruzic@users.noreply.github.com>
2020-03-01 18:13:05 +0100

    Delete mnozenje_matrica_sekvencijalni.py

--- mnozenje_matrica_sekvencijalni.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-"""Inicijalizacija matrica A i B"""
-A=np.random.rand(3,3)
-B=np.random.rand(3,3)
-"""Produkt matrica A i B"""
-C=np.matmul(A,B)
-"""Ispis matrice C"""
-print("Produkt matrica iznosi: ", C)
